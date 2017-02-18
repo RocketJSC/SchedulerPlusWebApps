2255b7ad787c1c35c1ea8079c3f222f0fcd0b34e
RocketJSC <mark.lindahl@gmail.com>
2017-02-17 20:39:06 -0600

    Latest

--- SP_Reports/src/SP_Reports/Startup.cs
+++ SP_Reports/src/SP_Reports/Startup.cs
@@ -101,6 +101,12 @@ namespace SP_Reports
             app.UseIdentity();
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
+            app.UseFacebookAuthentication(new FacebookOptions()
+            {
+                AppId = Configuration["Authentication:Facebook:AppId"],
+                AppSecret = Configuration["Authentication:Facebook:AppSecret"]
+            });
+
 
             app.UseMvc(routes =>
             {
